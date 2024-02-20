c17454a714e75f4a9b35b5fce2e69484e8e4d957
Farzeen Rashid <farzeenrashid2004@gmail.com>
2024-02-20 00:05:30 -0800

    Fix positioning of sketch-pad, title, and shake button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const shakeButton = document.querySelector('.left-side #shake-button');
-    const sketchPad = document.querySelector('.sketch-pad');
+    const shakeButton = document.querySelector('#left-side #shake-button');
+    const sketchPad = document.querySelector('#sketch-pad');
 
     shakeButton.addEventListener('click', function() {
         sketchPad.classList.add('shake');
