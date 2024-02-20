beb9d54a81adb81c6121e3df57341fc11344195a
Farzeen Rashid <farzeenrashid2004@gmail.com>
2024-02-19 23:32:06 -0800

    Fix shake animation and shake button functionality

---
+++ etch-a-sketch.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const shakeButton = document.querySelector('.left-side #shake-button');
+    const sketchPad = document.querySelector('.sketch-pad');
+
+    shakeButton.addEventListener('click', function() {
+        sketchPad.classList.add('shake');
+        sketchPad.addEventListener('animationend', function() {
+        sketchPad.classList.remove('shake');
+        });
+    });
+});
