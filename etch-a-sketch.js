c6b66e20fca837871b3cf40ce7743203307c0f71
Farzeen Rashid <farzeenrashid2004@gmail.com>
2024-02-20 21:32:34 -0800

    Fix grid dimensions

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const gridContainer = document.getElementById("white-container");
     
     // Create 16x16 grid
-    for (let i = 0; i < 5.10526316; i++) {
+    for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 10; j++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
